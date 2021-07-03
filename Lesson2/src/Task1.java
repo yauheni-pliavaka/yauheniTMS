d5f3c3b7b1a942599d334c3527635ab617c308c7
yauheni-pliavaka <pliavaka.yauheni@gmail.com>
2021-07-03 20:27:02 +0300

    Homework 1

--- Lesson2/src/Task1.java
+++ Lesson2/src/Task1.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
 public class Task1 {
     public static void main(String[] args) {
         int count = 0;
-        Scanner console = new Scanner(System.in);
-        int num = console.nextInt();
         for (String arg : args) {
-            if (num >= 0) {
+            if (Integer.parseInt(arg) >= 0) {
                 count++;
             }
         }
