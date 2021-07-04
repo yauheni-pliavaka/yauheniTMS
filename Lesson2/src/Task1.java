9f31a272af6cd633d9f29551c5f1237cb250b814
yauheni-pliavaka <pliavaka.yauheni@gmail.com>
2021-07-04 12:06:40 +0300

    Homework 1

--- Lesson2/src/Task1.java
+++ Lesson2/src/Task1.java
@@ -2,7 +2,7 @@ public class Task1 {
     public static void main(String[] args) {
         int count = 0;
         for (String arg : args) {
-            if (Integer.parseInt(arg) >= 0) {
+            if (Integer.parseInt(arg) > 0) {
                 count++;
             }
         }
