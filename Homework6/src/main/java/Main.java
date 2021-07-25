d587692304affa7b79529621d741f55fc943f0d4
yauheni-pliavaka <pliavaka.yauheni@gmail.com>
2021-07-25 15:20:24 +0300

    Homework 6

--- Homework6/src/main/java/Main.java
+++ Homework6/src/main/java/Main.java
@@ -23,7 +23,8 @@ public class Main {
     public static ArrayList<Person> initPersonList() {
         ArrayList<Person> personList = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
-            Address address = new Address(Calculation.randomCountry(), Calculation.randomCity(Calculation.randomCountry()));
+            String randomCountry = Calculation.randomCountry();
+            Address address = new Address(randomCountry, Calculation.randomCity(randomCountry));
             personList.add(new Person("name" + i, Calculation.randomAge(),
                     Calculation.randomSex(), address));
             System.out.println(personList.get(i));
