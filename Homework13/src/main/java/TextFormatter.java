6b8386540a9f6857b0740b128ae1aabfb68867a4
yauheni-pliavaka <pliavaka.yauheni@gmail.com>
2021-08-28 14:02:13 +0300

    домашка 13 исправленная

--- Homework13/src/main/java/TextFormatter.java
+++ Homework13/src/main/java/TextFormatter.java
@@ -1,10 +1,10 @@
 public final class TextFormatter {
     public static int getCountWords(String str) {
-        return str.split(" ").length;
+        return str.split("[^а-яА-Я]").length;
     }
 
     public static boolean hasPalindrome(String str) {
-        for (String s : str.split(" ")) {
+        for (String s : str.split("[^а-яА-Я]")) {
             if (s.equalsIgnoreCase(new StringBuilder(s).reverse().toString())) {
                 return true;
             }
